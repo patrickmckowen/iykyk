faa80a58c91df8b38b74ce99dd493ae5d9586cb3
patrickmckowen <patrick.mckowen@gmail.com>
2024-08-21 21:22:00 -0700

    Initial Commit

---
+++ iykykTests/iykykTests.swift
@@ -0,0 +1,16 @@
+//
+//  iykykTests.swift
+//  iykykTests
+//
+//  Created by Patrick McKowen on 8/21/24.
+//
+
+import Testing
+
+struct iykykTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
