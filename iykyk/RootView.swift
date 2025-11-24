048493443cd418268f4fdb53352af658996b2510
patrickmckowen <patrick.mckowen@gmail.com>
2025-11-23 22:20:36 -0800

    refine nav icon

--- iykyk/RootView.swift
+++ iykyk/RootView.swift
@@ -32,7 +32,7 @@ struct RootView: View {
                 }
             }
             
-            Tab("Play", systemImage: "circle.square") {
+            Tab("Play", systemImage: "hand.rays") {
                 NavigationStack {
                     PuzzleLibraryView(mode: .play)
                         .navigationDestination(for: Puzzle.self) { puzzle in
